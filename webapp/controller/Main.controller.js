375f4a546adccd92baaa27943281e54dc86a9555
CPUCloudCode <lennigaming8@gmail.com>
2023-11-13 11:22:17 +0100

    Translatable Texts (i18n)

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -23,7 +23,13 @@ sap.ui.define([
             },
 
             onShowHello: function () {
-                MessageToast.show("Hello World")
+                // read msg from i18n model
+                const oBundle = this.getView().getModel("i18n").getResourceBundle();
+                const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+                const sMsg = oBundle.getText("helloMsg", [sRecipient])
+
+                // show message
+                MessageToast.show(sMsg)
             }
         });
     });
