38e26e5b329878aee1541dd5f7a8f9da5fc0e515
CPUCloudCode <lennigaming8@gmail.com>
2023-11-13 11:13:26 +0100

    JSON Model

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,16 +1,25 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, JSONModel) {
         "use strict";
 
         return Controller.extend("zfhwedel.salesFreestyle.zfhwedelui5demo.controller.Main", {
+            // Called when view is initialized
             onInit: function () {
-
+                // set data model on view
+                const oData = {
+                    recipient: {
+                        name: "World"
+                    }
+                }
+                const oModel = new JSONModel(oData);
+                this.getView().setModel(oModel);
             },
 
             onShowHello: function () {
