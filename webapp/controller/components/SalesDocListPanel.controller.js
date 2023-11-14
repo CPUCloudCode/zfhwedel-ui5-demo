6c1fdc5af2e6edd2a57c2b6984c34aa422ba7ded
CPUCloudCode <lennigaming8@gmail.com>
2023-11-14 10:08:13 +0100

    Filtering

--- webapp/controller/components/SalesDocListPanel.controller.js
+++ webapp/controller/components/SalesDocListPanel.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
- ], (Controller) => {
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+ ], (Controller, Filter, FilterOperator) => {
     "use strict";
  
     return Controller.extend("ui5.walkthrough.controller.components.SalesDocListPanel", {
@@ -17,6 +19,23 @@ sap.ui.define([
 			oRouter.navTo("SalesDocDetail", {
             salesDocPath: window.encodeURIComponent(oItem.getBindingContext("salesDocs").getPath().substr(1))
          });
+       },
+
+       onFilterSalesDocs: function(oEvent) {
+         // build filter array
+			const aFilter = [];
+			const sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("SalesDoc", FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			const oList = this.byId("salesDocsList");
+			const oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
        }
+
+
+
     });
  });
